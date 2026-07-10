88d9eab20b6c6deb73f6c8ce977562c17f886cdc
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-10 14:01:29 +0300

    Изменения: добавлены шаблоны страниц логина, авторизации, изменения профиля + начальные стили к ним (ВНИМАНИЕ изменен базовай шаблон а именно пропорции центрального блока)

--- config/urls.py
+++ config/urls.py
@@ -4,6 +4,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+from django.contrib.auth import views as auth_views
 from django.views.generic import TemplateView
 
 urlpatterns = [
@@ -11,6 +12,9 @@ urlpatterns = [
     path("users/", include("users.urls")),
     # TODO: временная страница для проверки base.html
     path("test/", TemplateView.as_view(template_name="test_page.html")),
+    path("login/", auth_views.LoginView.as_view(template_name="login_page.html"), name="login"),
+    path("profile_edit/", TemplateView.as_view(template_name="profile_edit_page.html")),
+    path("signup/", TemplateView.as_view(template_name="signup_page.html")),
 ]
 
 if settings.DEBUG:
