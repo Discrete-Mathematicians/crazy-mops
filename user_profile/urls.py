efe540bcea259568cbab8d64d61df72adc57bff9
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-11 00:10:12 +0300

    Изменения: реализован просмотр профиля юзера (own/чужой) с флагом can_manage для владельческих кнопок. Список питомцев временно пуст, Поправлены ссылки в навбаре base.html (Моя страница, Войти, Регистрация) + контракт по профилю

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from user_profile import views
 
-app_name = 'profile'
+app_name = 'profiles'
 
 urlpatterns = [
     path('<int:pk>/', views.profile_detail, name='detail'),
