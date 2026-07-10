50bd0f3aa7715cf81b4c92b34532f19359e6b4fd
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-11 00:11:27 +0300

    code style fix

--- user_profile/urls.py
+++ user_profile/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
+
 from user_profile import views
 
-app_name = 'profiles'
+app_name = "profiles"
 
 urlpatterns = [
-    path('<int:pk>/', views.profile_detail, name='detail'),
+    path("<int:pk>/", views.profile_detail, name="detail"),
 ]
