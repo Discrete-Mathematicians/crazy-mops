b75be0e9f5a2f97880057766ab9715e66dcc496a
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-10 20:25:42 +0300

    Пробежуточные изменения profile

---
+++ user_profile/views.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404, render
+
+def profile_detail(request, pk):
+    User = get_user_model()
+    profile_user = get_object_or_404(User, pk=pk)
+    
+    pets = profile_user.pet_set.all()
+    
+    can_manage = (
+        request.user.is_authenticated
+        and (request.user == profile_user or request.user.is_staff)
+    )
+    
+    context = {
+        'profile_user': profile_user,
+        'pets': pets,
+        'can_manage': can_manage,
+    }
+    return render(request, 'profile/profile_detail.html', context)
+    
