68db9fac0909a008daf7267c36ad67d378573854
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-12 16:09:14 +0300

    feat(infra): подключение к PostgreSQL через переменные окружения

--- config/settings.py
+++ config/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -87,8 +90,12 @@ WSGI_APPLICATION = "config.wsgi.application"
 
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql",
+        "NAME": os.environ.get("POSTGRES_DB", "crazy_mops"),
+        "USER": os.environ.get("POSTGRES_USER", "crazy_mops"),
+        "PASSWORD": os.environ.get("POSTGRES_PASSWORD", "crazy_mops"),
+        "HOST": os.environ.get("POSTGRES_HOST", "localhost"),
+        "PORT": os.environ.get("POSTGRES_PORT", "5432"),
     }
 }
 
