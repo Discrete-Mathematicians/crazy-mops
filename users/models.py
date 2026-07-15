1415700b715659a5ef7a842ba91a4de1a6bfea4f
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-15 11:32:06 +0300

    feat: имя и фамилия владельца с фолбэком на display_name + передача csrf  в карточки

--- users/models.py
+++ users/models.py
@@ -48,6 +48,10 @@ class User(AbstractBaseUser, PermissionsMixin):
         if not self.display_name:
             self.display_name = self.login
         super().save(*args, **kwargs)
+    
+    def get_full_name(self):
+        """Имя и фамилия через пробел; пустая строка, если оба не заполнены."""
+        return f"{self.first_name} {self.last_name}".strip()
 
     def __str__(self):
         return self.display_name or self.login
