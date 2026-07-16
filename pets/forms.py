85939e8366e4ba30c9889da7d23640dafa7f634f
gdi24u211 <92597611+Barazded@users.noreply.github.com>
2026-07-17 01:51:22 +0300

    feat: форма питомца с дропбоксом

--- pets/forms.py
+++ pets/forms.py
@@ -7,7 +7,10 @@ class PetForm(forms.ModelForm):
     class Meta:
         model = Pet
         fields = ["name", "avatar", "pet_type", "breed", "birthday", "sex", "eye_color", "coat_color"]
-        widgets = {"birthday": forms.DateInput(attrs={"type": "date"}, format="%Y-%m-%d")}
+        widgets = {
+            "birthday": forms.DateInput(attrs={"type": "date"}, format="%Y-%m-%d"),
+            "avatar": forms.FileInput(attrs={"class": "dropzone-square__input"}),
+        }
 
     def clean_name(self):
         name = self.cleaned_data["name"].strip()
